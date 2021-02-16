2e8708d430606ddefa80652adf2ce2ac4182eda4
Thor Andresen <thorg3@gmail.com>
2021-02-16 13:55:14 +0100

    xd

--- Assets/Scripts/Harmonien.cs
+++ Assets/Scripts/Harmonien.cs
@@ -6,6 +6,7 @@ public class Harmonien : MonoBehaviour
 {
     [SerializeField] private GameObject field;
     private Vector3 invMat;
+    private string text = "Please approach the landing strip";
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,14 @@ public class Harmonien : MonoBehaviour
     {
         Matrix4x4 mat = gameObject.transform.localToWorldMatrix;
         invMat = field.transform.worldToLocalMatrix.MultiplyPoint(new Vector3(mat.m03, mat.m13, mat.m23));
+
+        if((invMat.x < 0f && invMat.x > -2f) && (invMat.y < 0f && invMat.y > -4f) && (invMat.z < 4f && invMat.z > 0f))
+        {
+            text = "Prepare to land";
+        } else
+        {
+            text = "Please approach the landing strip";
+        }
     }
 
     private void OnGUI()
@@ -26,6 +35,7 @@ public class Harmonien : MonoBehaviour
         GUI.skin.label.fontSize = 30;
         GUI.color = Color.red;
         GUI.Label(new Rect(10, 10, 1000, 300), $"Local position: {invMat.x}, {invMat.y}, {invMat.z}");
+        GUI.Label(new Rect(10, 50, 1000, 300), $"Local position: {text}");
     }
 
 }
