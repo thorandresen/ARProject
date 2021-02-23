cd3d03f15ee40e722e3a7329e1abd189c4a197e7
Thor Andresen <thorg3@gmail.com>
2021-02-23 14:47:38 +0100

    laursen sutter

--- Assets/Scripts/Assignment21c.cs
+++ Assets/Scripts/Assignment21c.cs
@@ -6,30 +6,40 @@ public class Assignment21c : MonoBehaviour
 {
     [SerializeField] private GameObject Ox;
     [SerializeField] private GameObject As;
+    private Vector3 pos1;
+    private Vector3 pos2;
+    public float speed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    bool i = false;
+    bool towardsAs = false;
     // Update is called once per frame
     void Update()
     {
-        //Vector3 targetDirection = Ox.transform.position - gameObject.transform.position;
+        pos1 = Ox.transform.position;
+        pos2 = As.transform.position;
 
-
-        //Vector3 rotate = Vector3.RotateTowards(gameObject.transform.forward, targetDirection, 1.0f, 0.0f);
-        //gameObject.transform.rotation = Quaternion.LookRotation(rotate);
-
-   
-        if (i)
+        if(Mathf.Abs(transform.position.x - pos2.x) < 1 && Mathf.Abs(transform.position.y - pos2.y) < 1 && Mathf.Abs(transform.position.z - pos2.z) < 1)
         {
-            //gameObject.transform.position = gameObject.transform.position + 
+            towardsAs = true;
+        } else if (Mathf.Abs(transform.position.x - pos1.x) < 1 && Mathf.Abs(transform.position.y - pos1.y) < 1 && Mathf.Abs(transform.position.z - pos1.z) < 1)
+        {
+            towardsAs = false;
+        }
+        
+        if(towardsAs)
+        {
+            transform.LookAt(pos1);
         } else
         {
-
+            transform.LookAt(pos2);
         }
-    }
+        
+        transform.rotation *= Quaternion.Euler(90, 0, 0);
+        transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time* speed, 1.0f));
+     }
 }
